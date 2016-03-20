be2568ee9146d15f79b37846b91f9f96773e63ff
Sam Robinson <pinkstarsr11@gmail.com>
2016-03-19 18:28:37 -0700

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Reflex_Test {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(515, 382);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click To Start";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(515, 382);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(311, 218);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reflex Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Reflex_Test {
+    public partial class Form1:Form {
+        private Random fakeRng = new Random(DateTime.Now.Millisecond);
+        private int status = 0;
+        private int StartTime;
+        private Thread NiceMeme;
+        public Form1() {
+            InitializeComponent();
+        }
+        private void label1_MouseDown(object sender,MouseEventArgs e) {
+            if(status == 0) {
+                NiceMeme = new Thread(NiceMemeDelegate);
+                NiceMeme.Start();
+                label1.Text = "Click When The Background Turns Green";
+                status = 1;
+            } else if(status == 1) {
+                NiceMeme.Abort();
+                label1.Text = "You Clicked Too Early, Click to Try Again";
+                status = 0;
+            } else if(status == 2) {
+                int FinishTime = Convert.ToInt32(DateTime.Now.TimeOfDay.TotalMilliseconds) - StartTime;
+                label1.Text = "" + FinishTime + " milliseconds, Click To Try Again";
+                status = 0;
+                this.BackColor = Color.Red;
+            }
+        }
+        private void NiceMemeDelegate() {
+            Thread.Sleep(fakeRng.Next(4000, 9000));
+            status = 2;
+            this.BackColor = Color.Green;
+            StartTime = Convert.ToInt32(DateTime.Now.TimeOfDay.TotalMilliseconds);
+        }
+    }
+}
